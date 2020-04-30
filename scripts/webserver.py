13f85fd3ecd4ef6a87c4206df4a21c9be403ab19
Yasushi Oshima <oosmyss@gmail.com>
2020-04-30 10:13:09 +0900

    Refactoring to conform with PEP8.

--- scripts/webserver.py
+++ scripts/webserver.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-import rospy, os
+
+import rospy
+import os
 import SimpleHTTPServer
+
 
 def kill():
     os.system('kill -KILL ' + str(os.getpid()))
+
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(__file__) + '/../contents')
